cc6600e0b9a04a082f2efe249c9e90ee6247cebf
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-19 22:07:06 +0200

    Update index.tsx

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,16 +1,33 @@
-import Header from "@/components/layout/Header";
-import Button from "@/components/common/Button";
+import React from "react";
+import { UserProps } from "../../interfaces";
+import UserCard from "../../components/common/UserCard";
 
-const UsersPage: React.FC = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow p-6">
-        <h1 className="text-3xl font-bold mb-4">Users</h1>
-        <Button title="Add User" />
-      </main>
-    </div>
-  )
+interface UsersPageProps {
+  posts: UserProps[];
 }
 
-export default UsersPage;
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Users</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {posts.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
