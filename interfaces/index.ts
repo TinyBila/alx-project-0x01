3cd78b1d148ea1fc829026ef5e4fa4b87b8c6b79
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-19 22:40:02 +0200

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,13 +1,32 @@
-export interface PostData {
-  userId: number;
+// Already have PostProps, PostData, PostModalProps...
+
+export interface UserData {
   id?: number;
-  title: string;
-  body: string;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
 }
 
-export interface PostModalProps {
+export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (post: PostData) => void;
+  onSubmit: (user: UserData) => void;
 }
 
 export interface PostData {
