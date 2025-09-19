a0a0bf9566cb74495bf0133260646d633811b9ea
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-19 21:32:16 +0200

    Update index.tsx

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
 import Button from "@/components/common/Button";
 
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Users</h1>
-      <Button title="Add User" />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-6">
+        <h1 className="text-3xl font-bold mb-4">Users</h1>
+        <Button title="Add User" />
+      </main>
     </div>
   )
 }
