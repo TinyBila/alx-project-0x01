bac118bc5bd137b923ad1dcfc716e257a42ac7d7
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-19 21:23:24 +0200

    Create index.tsx

---
+++ pages/users/index.tsx
@@ -0,0 +1,12 @@
+import Button from "@/components/common/Button";
+
+const UsersPage: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Users</h1>
+      <Button title="Add User" />
+    </div>
+  )
+}
+
+export default UsersPage;
