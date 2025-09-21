15d6ada1c54d0ba311002255bac130cd1302d21d
TinyBila <bilatinym@gmail.com>
2025-09-21 21:10:21 +0200

    updated

--- alx-project-0x01/components/common/Button.tsx
+++ alx-project-0x01/components/common/Button.tsx
@@ -1,1 +1,9 @@
+const Button: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+      {title}
+    </button>
+  )
+}
 
+export default Button;
